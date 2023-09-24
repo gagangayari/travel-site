da43fbfcc11ce6931056a706cfd45be7b12c35a1
gagangayari <gagangayari@gmail.com>
2023-09-24 20:31:17 +0530

    Modified design using bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatButtonModule} from '@angular/material/button'
 import { MatDialogModule } from '@angular/material/dialog';
-
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -21,7 +21,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
